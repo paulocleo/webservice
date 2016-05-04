5e69a74e0ca247e9d65b24f9fe8afbdd214a2314
Paulo Cruz <paulocac_1@hotmail.com>
2016-05-03 21:47:01 -0300

    add lib

---
+++ consultas.php
@@ -0,0 +1,37 @@
+<?php
+//Arquivo da classe NuSOAP
+require_once('lib/nusoap.php');
+
+//Criação da instancia do servidor
+$server = new soap_server;
+//Inicia o suporte a WSDL
+$server->configureWSDL('server.webservice', 'urn:server.webservice');
+$server->wsdl->schemaTargetNamespace = 'urn:server.webservice';
+//Registro do Método
+$server->register('consulta_processo', //Nome do método
+array('num_processo' => 'xsd:string'),
+array('return' => 'xsd:string'), //Parametros de saida
+'urn:server.webservice', //namespace
+'urn:server.webservice#consulta',
+'rpc', //style
+'encoded', //use
+'Retorna consulta do processo' //Documentação do serviço
+);
+
+//Métodos
+
+	function listar_onibus($origem, $destino, $via_conexao)
+	{		
+		// $conn = mysql_connect('mysql.hostinger.com.br', 'u974275456_bus', 'cruz2401');
+		// if (!$conn) {
+			// die('Could not connect: ' . mysql_error());
+		// }
+		// //echo 'Connected database successfully';
+		// mysql_select_db("u974275456_bus");
+		
+	}
+
+	//Requisição para uso do serviço
+	$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
+	$server->service($HTTP_RAW_POST_DATA);
+?>
