af4c755a35463ad71d8a71396bd666859c96c1cc
Paulo Cruz <paulocac_1@hotmail.com>
2016-05-03 22:48:38 -0300

    editado index

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ array('return' => 'xsd:string'), //Parametros de saida
 	{
 		include('arquivos/pdf2text.php');
 		// $result = pdf2text ('aplicacao.jt.jus.br/Diario_J_01.pdf');
-		$result = pdf2text ('arquivos/dj.pdf');
+		$result = pdf2text ('arquivo/dj.pdf');
 		if($result == "")
 			$result = "Não encontrado";
 		return $result;
